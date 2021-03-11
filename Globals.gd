1ec71255691ccf7302c15aacc1afe62f96fe6be3
ACB-prgm <63984796+ACB-prgm@users.noreply.github.com>
2021-03-11 08:57:15 -0500

    Initial Commit

---
+++ Globals.gd
@@ -0,0 +1,4 @@
+extends Node
+
+
+var camera = null
---
+++ ShakeDemoTSCN/MainShake.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _on_Button_pressed():
+	Globals.camera.shake(25)
+
+
+func _on_Button2_pressed():
+	Globals.camera.shake(100)
+
+
+func _on_Button3_pressed():
+	Globals.camera.shake(300, 2, 300)
